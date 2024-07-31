3a47298ec16153830e9d4e9cbb0d81dee4a81065
Shruthi Yenamagandla <yshruthi.14@gmail.com>
2024-07-31 13:56:20 -0500

    started frontend layout

--- app/page.js
+++ app/page.js
@@ -2,70 +2,126 @@
 import Image from "next/image";
 import {useState, useEffect} from 'react'
 import { firestore } from "@/firebase"
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Modal, Stack, TextField, Button } from "@mui/material";
 import { query, collection, getDocs} from "firebase/firestore";
+import { doc, getDoc, setDoc, deleteDoc } from "firebase/firestore";
 
 
 export default function Home() {
   // Inventory Management Helper Functions
-  const [inventory, setInventory] = useState([]) // default value is empty array
-  const [open, setOpen] = useState(false)       // default valie is false
-  const [itemName, setItemName] = useState('') // default value is empty string
+  const [inventory, setInventory] = useState([]); // default value is empty array
+  const [open, setOpen] = useState(false); // default value is false
+  const [itemName, setItemName] = useState(''); // default value is empty string
 
-  const updateInventory = async () => {      // async: prevents the code from being blocked while fetching so website wont freeze
-    const snapshot = query(collection(firestore, 'inventory'))
-    const docs = await getDocs(snapshot)
-    const inventoryList = []
-    docs.forEach((doc)=>{
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'));
+    const docs = await getDocs(snapshot);
+    const inventoryList = [];
+    docs.forEach((doc) => {
       inventoryList.push({
         name: doc.id,
         ...doc.data(),
-      })
-    })
-    setInventory(inventoryList)
-  }
+      });
+    });
+    setInventory(inventoryList);
+  };
 
   const addItem = async (item) => {
-    const docRef = doc(collection(firestore, 'inventory'), item)
-    const docSnap = await getDoc(docRef)
+    const docRef = doc(collection(firestore, 'inventory'), item);
+    const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
-      const {quantity} = docSnap.data()
-      await setDoc(docRef, {quantity: quantity + 1})
-    } 
-    else {
-      await setDoc(docRef, {quantity: 1})
+      const { quantity } = docSnap.data();
+      await setDoc(docRef, { quantity: quantity + 1 });
+    } else {
+      await setDoc(docRef, { quantity: 1 });
     }
-    await updateInventory()
-  }
+    await updateInventory();
+  };
 
   const removeItem = async (item) => {
-    const docRef = doc(collection(firestore, 'inventory'), item)
-    const docSnap = await getDoc(docRef)
+    const docRef = doc(collection(firestore, 'inventory'), item);
+    const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
-      const {quantity} = docSnap.data()
-      if (quantity === 1) 
-      {
-        await deleteDoc(docRef)
+      const { quantity } = docSnap.data();
+      if (quantity === 1) {
+        await deleteDoc(docRef);
+      } else {
+        await setDoc(docRef, { quantity: quantity - 1 });
       }
-      else 
-      {
-        await setDoc(docRef, {quantity: quantity - 1 })
-      }
-    } 
-    await updateInventory()
-  }
+    }
+    await updateInventory();
+  };
 
   useEffect(() => {
-    updateInventory()
-  }, [])
+    updateInventory();
+  }, []);
 
-  const handleOpen = () => setOpen(true)
-  const handleClose = () => setOpen(false)
-
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
-    <Box> 
-      <Typography variant="h2"> Inventory Management </Typography> 
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      gap={2}
+    >
+      <Modal open={open} onClose={handleClose}>
+        <Box
+          position="absolute"
+          top="50%"
+          left="50%"
+          width={400}
+          bgcolor="white"
+          border="2px solid #000"
+          boxShadow={24}
+          p={4}
+          display="flex"
+          flexDirection="column"
+          gap={3}
+          sx={{
+            transform: 'translate(-50%, -50%)',
+          }}
+        >
+          <Typography variant="h6">Add Item</Typography>
+          <Stack width="100%" direction="row" spacing={2}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              value={itemName}
+              onChange={(e) => {
+                setItemName(e.target.value);
+              }}
+            />
+            <Button 
+              variant="outlined" 
+              onClick={()=>{
+                addItem(itemName)
+                setItemName('')
+                handleClose()
+              }} 
+            >Add</Button>
+          </Stack>
+        </Box>
+      </Modal>
+
+      <Button 
+        variant = "contained"
+        onClick={() => {
+          handleOpen()
+        }} > 
+        Add New Item 
+      </Button>
+
+      <Box border="1px solid #333">
+        <Box width="800px" height="100px" bgcolor="#ADD8E6" display="flex" justifyContent="center" alignItems="center">
+          <Typography variant="h2" color="#333"> Inventory Items </Typography>
+        </Box>
+      </Box>
+
     </Box>
   );
 }
